7904a7f7d303cfa8c9eb4e309c46f8ef91c19fdc
supermancbs <clark.sacktor@gmail.com>
2016-05-02 00:19:50 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result={}
+  data.each do |attributes, attribute_data|
+  	attribute_data.each do |attribute, names|
+  		names.each do |name|
+  			if result.has_key?(name)==false
+  				result[name] = {}
+  			end 
+  			if result[name].has_key?(attributes)==false
+  				result[name][attributes] = [attribute.to_s]
+  			else 
+  				result[name][attributes] << attribute.to_s
+  			end
+  		end 
+  	end 
+  end 
+  result
 end
+
